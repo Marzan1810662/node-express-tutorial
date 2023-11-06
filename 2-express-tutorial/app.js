028250b8e6fb3644c7f399907fa9d0ee08b6e60d
Marzan1810662 <hassanmarzan17@gmail.com>
2023-11-06 21:31:14 +0600

    http req object

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,8 +1,34 @@
 const http = require('http')
 
-const server = http.createServer((req,res)=>{
-    console.log('User hit the server');
-    res.end('Home page')
+const server = http.createServer((req, res) => {
+    // console.log('User hit the server');
+    console.log(req.url);
+    const url = req.url
+
+    //home page
+    if (url === '/') {
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        })
+        res.write('<h1>Home Page</h1>')
+        res.end()
+    }
+    //about page
+    else if (url === '/about') {
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        })
+        res.write('<h1>About Page</h1>')
+        res.end()
+    }
+    //404
+    else {
+        res.writeHead(404, {
+            'Content-Type': 'text/html'
+        })
+        res.write('<h1>Page Not Found!</h1>')
+        res.end()
+    }
 })
 
 server.listen(5000)
